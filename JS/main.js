a10656067e1975e05e1eb7a3b0481d2470640aec
Kissu..(:💗 <163384034+spywithcode@users.noreply.github.com>
2024-05-05 00:10:34 +0530

    assets

---
+++ JS/main.js
@@ -0,0 +1,108 @@
+/*----------------- Typing Animation --------------------*/
+// var typed = new Typed(".typing", {
+//     Strings: ["Web Designer", "Web Developer", "Graphic Designer", "YouTuber"],
+//     typeSpeed: 100,
+//     BackSpeed: 60,
+//     loop: true
+// });
+
+/*----------------- Aside --------------------*/
+const nav = document.querySelector(".nav"),
+    navList = nav.querySelectorAll("li"),
+    totalNavList = navList.length,
+    allSection = document.querySelectorAll(".section"),
+    totalSection = allSection.length;
+
+for (let i = 0; i < totalNavList; i++) {
+    const a = navList[i].querySelector("a");
+    a.addEventListener("click", function () {
+        removeBackSection();
+        for (let j = 0; j < totalNavList; j++) {
+            if (navList[j].querySelector("a").classList.contains("active")) {
+                addBackSection(j);
+                // allSection[j].classList.add("back-section");
+            }
+            navList[j].querySelector("a").classList.remove('active');
+        }
+        this.classList.add('active');
+        showSection(this);
+        if (window.innerWidth <= 1200) {
+            asideSectionTogglerBtn();
+        }
+    })
+}
+
+function removeBackSection() {
+    for (let i = 0; i < totalSection; i++) {
+        allSection[i].classList.remove("back-section");
+    }
+}
+
+function addBackSection(num) {
+    allSection[num].classList.add("back-section");
+}
+
+function showSection(element) {
+    for (let i = 0; i < totalSection; i++) {
+        allSection[i].classList.remove("active");
+    }
+    const target = element.getAttribute("href").split("#")[1];
+    document.querySelector("#" + target).classList.add("active")
+}
+
+function updateNav(element) {
+    for (let i = 0; i < totalNavList; i++) {
+        navList[i].querySelector("a").classList.remove("active");
+        const target = element.getAttribute("href").split("#")[1];
+        if (target === navList[i].querySelector("a").getAttribute("href").split("#")[1]) {
+            navList[i].querySelector("a").classList.add("active");
+        }
+    }
+}
+
+document.querySelector(".hire-me").addEventListener("click", function () {
+    const sectionIndex = this.getAttribute("data-setion-index");
+    // console.log(sectionIndex);
+    showSection(this);
+    updateNav(this);
+    removeBackSection();
+    addBackSection(sectionIndex);
+});
+
+const navTogglerBtn = document.querySelector(".nav-toggler"),
+    aside = document.querySelector(".aside");
+navTogglerBtn.addEventListener("click", () => {
+    asideSectionTogglerBtn();
+})
+
+function asideSectionTogglerBtn() {
+    aside.classList.toggle("open");
+    navTogglerBtn.classList.toggle("open");
+    for (let i = 0; i < totalSection; i++) {
+        allSection[i].classList.toggle("open");
+    }
+}
+
+// contact 
+
+const contactForm = document.querySelector("contact-form");
+
+function sendEmail(){
+    Email.send({
+        Host : "smtp.elasticemail.com",
+        Username : "spywithcode@gmail.com",
+        Password : "B48DF540963BDEEE20E128137960E7EB24B5",
+        To : 'spywithcode@gmail.com',
+        From : "spywithcode@gmail.com",
+        Subject : "This is the subject",
+        Body : "And this is the body"
+    }).then(
+      message => alert(message)
+    );
+}
+
+contactForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    sendEmail();
+})
